fa5f7f3e11d1fa06b7f772b5156f9c28021c1cf6
MashoodCk <mashoodck77@gmail.com>
2024-11-18 19:43:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>NutriMind</h1>
+      <iframe width='500' height='400' src='https://embed.coggle.it/diagram/Zzod2LznpGyxYGmA/ff77b2f2890917790804ae871f2b9afc8a35e685b0c4220ded2942fdacf206d7' frameborder='5' allowfullscreen></iframe>
+      <iframe width='500' height='400' src='https://embed.coggle.it/diagram/ZzW2cgaoDqinMfp7/9f8caf089053876a94af20b20a609a36eb66a916528bb54816511fcf585a9123' frameborder='5' allowfullscreen></iframe>
     </div>
   );
 }
